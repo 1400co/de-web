a70993b5cb55a4b0be659c02420e42bcfce4208c
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-04-08 08:52:03 -0500

    agregado pixel de facebook

--- digital-experts-web/src/app/app.component.ts
+++ digital-experts-web/src/app/app.component.ts
@@ -1,21 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import {  PixelService } from 'ngx-multi-pixel';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule ],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'digital-experts-web';
-  
-  constructor() {
 
+  constructor(private pixel: PixelService) {
+
+  }
+
+  ngOnInit(): void {
+    this.onConsent();
+  }
+
+  onWhatsAppContact(): void {
+  }
+
+  onConsent(): void {
+    this.pixel.initialize(['1676952112730164']);
   }
 
 }
